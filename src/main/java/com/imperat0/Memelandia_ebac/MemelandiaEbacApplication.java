479c309d80e39466619bf713a4d5e5778a243c3e
Imperat0 <imperato06@gmail.com>
2025-06-08 21:13:01 -0300

    ebac projeto final

---
+++ src/main/java/com/imperat0/Memelandia_ebac/MemelandiaEbacApplication.java
@@ -0,0 +1,13 @@
+package com.imperat0.Memelandia_ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemelandiaEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemelandiaEbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imperat0/Memelandia_ebac/MemelandiaEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imperat0.Memelandia_ebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemelandiaEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
